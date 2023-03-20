3b2a4c695de01699cf3897debeb5ceb9bac6afd5
tak916 <aaronlee916@qq.com>
2023-03-20 14:11:25 +0800

    component

---
+++ React/MyFirstReactComponent/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import {} from 'react-bootstrap'
+import Toast from './Toast';
+import './App.css';
+
+function App() {
+  return (
+    <Toast />
+  );
+}
+
+export default App;
---
+++ React/MyFirstReactComponent/src/Toast.js
@@ -0,0 +1,4 @@
+import {} from 'react-bootstrap'
+export default function Toast(){
+    return(<h1>This is a Toast Component!</h1>)
+}
